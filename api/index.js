02ed99fc563e343c79eaaf45ff1536758a5a9448
saeedehsp <s.sadeghpour97@gmail.com>
2018-08-20 14:14:52 +0430

    feat: reading from static file

--- api/index.js
+++ api/index.js
@@ -27,15 +27,53 @@ app.listen(3001, function () {
 
 app.use(bodyparser.json());
 
-
+const im = require('./fields.js').exam;
+let exam = im;
+const field= {
+    std_number: im.fields.std_number.title,
+    name: im.fields.name.title,
+    family_name: im.fields.family_name.title,
+    course_code: im.fields.course_code.title,
+    course_name: im.fields.course_name.title,
+    date: im.fields.date.title,
+    grade: im.fields.grade.title,
+    semester: im.fields.semester.title,
+    prof_name: im.fields.prof_name.title,
+    prof_family_name: im.fields.prof_family_name.title,
+    seat: im.fields.seat.title,//?
+    location: im.fields.location.title,
+};
 function imported(res, jsonArray) {
+    var array = [];
     console.log(jsonArray);
     res.json({error_code:0,err_desc:null, data: jsonArray});
+    for (var i=0; i < jsonArray.length; i++) {
+        array.push({
+            std_number : jsonArray[i].im.fields.std_number.title,
+            name : jsonArray[i].im.fields.name.title,
+            family_name : jsonArray[i].im.fields.family_name.title,
+            course_code : jsonArray[i].im.fields.course_code.title,
+            course_name : jsonArray[i].im.fields.course_name.title,
+            //date : jsonArray[i].exam.fields.date.title,
+            grade : jsonArray[i].im.fields.grade.title,
+            semester : jsonArray[i].im.fields.semester.title,
+            prof_name : jsonArray[i].im.fields.prof_name.title,
+            prof_family_name : jsonArray[i].im.fields.prof_family_name.title,
+            seat : jsonArray[i].im.fields.seat.title,//?
+            location : jsonArray[i].im.fields.location.title
+        });
+    }
+    console.log(array);
+    res.send({error_code:0,err_desc:'', data: array});
 }
+
+
 
 
 app.post('/import', upload, async function (req, res) {
 
+    var exceltojson;
+    
     switch (path.extname(req.file.originalname)) {
         case '.csv':
             const jsonArray = await csv().fromFile(req.file.path);
@@ -43,96 +81,24 @@ app.post('/import', upload, async function (req, res) {
             return;
     
         case '.xls':
-            //exceltojson = xlsxtojson;
-            var exceltojson;
-            upload(req,res,function(err){
-                if(err){
-                     res.json({error_code:1,err_desc:err});
-                     return;
-                }
-                /** Multer gives us file info in req.file object */
-                if(!req.file){
-                    res.json({error_code:1,err_desc:"No file passed"});
-                    return;
-                }
-                /** Check the extension of the incoming file and
-                 *  use the appropriate module
-                 */
-                
-                    exceltojson = xlstojson;
-                
-               
-                try {
-                    exceltojson({
-                        input: req.file.path,
-                        output: null, //since we don't need output.json
-                        lowerCaseHeaders:true
-                    }, function(err,result){
-                        if(err) {
-                            return res.json({error_code:1,err_desc:err, data: null});
-                        }
-                        res.json({error_code:0,err_desc:null, data: result});
-                        console.log(result)
-                    });
-                } catch (e){
-                    res.json({error_code:1,err_desc:"Corupted excel file"});
-                }
-               
-            }) 
-            break;
-    
+            exceltojson = xlstojson;
         case '.xlsx':
-          //  exceltojson = xlstojson;
-            var exceltojson;
-            upload(req,res,function(err){
-                if(err){
-                     res.json({error_code:1,err_desc:err});
-                     return;
-                }
-                /** Multer gives us file info in req.file object */
-                if(!req.file){
-                    res.json({error_code:1,err_desc:"No file passed"});
-                    return;
-                }
-                /** Check the extension of the incoming file and
-                 *  use the appropriate module
-                 */
-              
-                    exceltojson = xlsxtojson;
-               
-               
-                try {
-                    exceltojson({
-                        input: req.file.path,
-                        output: null, //since we don't need output.json
-                        lowerCaseHeaders:true
-                    }, function(err,result){
-                        if(err) {
-                            return res.json({error_code:1,err_desc:err, data: null});
-                        }
-                        res.json({error_code:0,err_desc:null, data: result});
-                        console.log(result)
-                    });
-                } catch (e){
-                    res.json({error_code:1,err_desc:"Corupted excel file"});
-                }
-               
-            }) 
+            exceltojson = xlsxtojson;
             break;
         default:
             res.json({error_code:1,err_desc:'Wrong file type', data: null});
             return;
     }
 
-    // exceltojson({
-    //     input: req.file.path,
-    //     output: null, //since we don't need output.json
-    //     lowerCaseHeaders:true
-    // }, function(err,result){
-    //     if(err) {
-    //         return res.json({error_code:1,err_desc:err, data: null});
-    //     }
+    exceltojson({
+        input: req.file.path,
+        output: null, //since we don't need output.json
+        lowerCaseHeaders:true
+    }, function(err,result){
+        if(err) {
+            return res.json({error_code:1,err_desc:err, data: null});
+        }
 
-    //     imported(res, jsonArray);
-    // });
+        imported(res, result);
+    });
 })
